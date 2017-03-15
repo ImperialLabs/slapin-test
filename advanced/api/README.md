864723822bac599be56f42ac74f824931c68c69b
Levi Smith <levi@fynx.me>
2017-03-15 17:04:53 -0500

    adding readme, changing file name to match class, adding supervisor file

---
+++ advanced/api/README.md
@@ -0,0 +1,12 @@
+# SLAPIN - API Example
+
+## Prerequisites
+-   Docker 1.10 or later
+
+## Usage
+
+To start the container
+
+```bash
+docker run -d -p 8100:8100 --name slapin_api slapin_api
+```
--- advanced/api/slapin.rb
+++ advanced/api/slapin.rb
@@ -23,7 +23,7 @@ class Slapin < Sinatra::Application
     raise 'missing timestamp' unless params[:timestamp]
     raise 'missing command' unless params[:command]
     @params = params
-    get if params[:command] =~ /get/
+    search if params[:command] =~ /search/
     save if params[:command] =~ /save/
     hello_world if params[:command] =~ /hello_world/
   end
@@ -31,27 +31,29 @@ class Slapin < Sinatra::Application
   get '/info' do
     {
       help:
-        get: "Get info from brain, Search for all keys: `@bot get` - Search specific key for value: `@bot get $key`"
-        save: "Save info into brain: `@bot save $key $value`"
-        hello: "Hello World Command: `@bot hello world` returns `Hello World!`"
+        {
+          search: 'Search info from brain, Search for all keys: `@bot search` - Search specific key for value: `@bot search $key`"',
+          save: 'Save info into brain: `@bot save $key $value`',
+          hello: 'Hello World Command: `@bot hello world` returns `Hello World!`'
+        }
     }.to_json
   end
 
-  def get
+  def search
     if @params[:command].include? ' '
       command_arrary = @params[:command].split(' ')
-      return = get_value(command_arrary[1])
+      response = search_value(command_arrary[1])
     else
-      return = get_keys
+      response = search_keys
     end
-    speak('Search Return', 'Search Return', return)
+    speak('Search Return', 'Search Return', response)
   end
 
-  def get_keys
+  def search_keys
     HTTParty.get(@bot_url + '/v1/query_hash', headers: @headers)
   end
 
-  def get_value(key)
+  def search_value(key)
     text_array = @params[:text].split(' ')
     @headers['plugin'] = text_array[1]
     @headers['key'] = key
@@ -60,6 +62,12 @@ class Slapin < Sinatra::Application
 
   def save
     command_arrary = @params[:command].split(' ')
+    text_array = @params[:text].split(' ')
+    body = {
+      plugin: text_array[1],
+      key: command_arrary[0],
+      value: command_arrary[1]
+    }
     HTTParty.post(@bot_url + '/v1/save', headers: @headers, body: body)
   end
 
@@ -71,9 +79,11 @@ class Slapin < Sinatra::Application
     body = {
       channel: @params[:channel],
       attachments:
-        fallback: fallback,
-        title: title,
-        text: text
+        {
+          fallback: fallback,
+          title: title,
+          text: text
+        }
     }
     HTTParty.post(@bot_url + '/v1/attachment', headers: @headers, body: body)
   end
