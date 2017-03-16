ee14fd8b673051596fa1ced5ae2bea35bba74f83
Levi Smith <levi@fynx.me>
2017-03-16 17:23:13 -0500

    Adding/Updating Readme, changing app port, fixing some other start items

--- advanced/api/slapin.rb
+++ advanced/api/slapin.rb
@@ -2,9 +2,14 @@
 require 'json'
 require 'httparty'
 require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/config_file'
 
 # Test API Plugin
-class Slapin < Sinatra::Application
+class Slapin < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+  register Sinatra::ConfigFile
+
   set :environment, :production
 
   config_file 'environments.yml'
@@ -12,61 +17,55 @@ class Slapin < Sinatra::Application
 
   @headers = {}
 
-  def bot_url
-    @bot_url = ENV['BOT_URL'] ? ENV['BOT_URL'] : settings.bot_url
-  end
+  @bot_url = ENV['BOT_URL'] ? ENV['BOT_URL'] : settings.bot_url
 
-  get '/endpoint' do
+  post '/endpoint' do
     raise 'missing user' unless params[:user]
     raise 'missing channel' unless params[:channel]
     raise 'missing type' unless params[:type]
     raise 'missing timestamp' unless params[:timestamp]
     raise 'missing command' unless params[:command]
     @params = params
-    search if params[:command] =~ /search/
-    save if params[:command] =~ /save/
-    hello_world if params[:command] =~ /hello_world/
+    @command_array = @params[:command].split(' ')
+    @text_array = @params[:text].split(' ')
+    search if @command_array[0] == 'search'
+    save if @command_array[0] == 'save'
+    hello_world if @command_array[0] == 'hello'
   end
 
   get '/info' do
     {
       help:
         {
-          search: 'Search info from brain, Search for all keys: `@bot search` - Search specific key for value: `@bot search $key`"',
-          save: 'Save info into brain: `@bot save $key $value`',
-          hello: 'Hello World Command: `@bot hello world` returns `Hello World!`'
+          search: 'Search info from brain, Search for all keys: `@bot api search` - Search specific key for value: `@bot api search $key`"',
+          save: 'Save info into brain: `@bot api save $key $value`',
+          hello: 'Hello World Command: `@bot api hello world` returns `Hello World!`'
         }
     }.to_json
   end
 
   def search
-    if @params[:command].include? ' '
-      command_arrary = @params[:command].split(' ')
-      response = search_value(command_arrary[1])
-    else
-      response = search_keys
-    end
+    response = search_value if @params[:command].include? ' '
+    response = search_keys unless @params[:command].include? ' '
     speak('Search Return', 'Search Return', response)
   end
 
   def search_keys
+    @headers['plugin'] = @text_array[1]
     HTTParty.get(@bot_url + '/v1/query_hash', headers: @headers)
   end
 
-  def search_value(key)
-    text_array = @params[:text].split(' ')
-    @headers['plugin'] = text_array[1]
-    @headers['key'] = key
+  def search_value
+    @headers['plugin'] = @text_array[1]
+    @headers['key'] = @command_array[1]
     HTTParty.get(@bot_url + '/v1/query_key', headers: @headers)
   end
 
   def save
-    command_arrary = @params[:command].split(' ')
-    text_array = @params[:text].split(' ')
     body = {
-      plugin: text_array[1],
-      key: command_arrary[0],
-      value: command_arrary[1]
+      plugin: @text_array[1],
+      key: @command_array[1],
+      value: @command_array[2]
     }
     HTTParty.post(@bot_url + '/v1/save', headers: @headers, body: body)
   end
